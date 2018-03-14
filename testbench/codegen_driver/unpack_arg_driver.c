f6457a65b430ad2bb962ddb6fd6669702f5d33d2
patrickjohncyh <patrickjohncyh@icloud.com>
2018-03-14 01:20:41 +0000

    Verified that unpacking of arguments with a mix of arguemnt types (int and float) is done correctly. Tested with a driver and slave approach as driver is assumed to have packed parameters according to ABI

---
+++ testbench/codegen_driver/unpack_arg_driver.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+float f(float a);
+float g(float a,float b);
+float h(float a,int b, float c);
+float i(int a, float b, float c, int d, float e);
+float j(float a);
+float k(float a);
+
+
+
+int main(){
+
+	float a = 1.23;
+	float b = 4.5;
+	int c = 4;
+
+	if(f(a) != a + 5){
+		return 1;
+	}
+	if(g(a,b) != a + b){
+		return 1;
+	}
+	if(h(a,c,b) != a+c+b){
+		return 1;
+	}
+
+	if(i(c,a,a,c,b) != c+a+a+c+b){
+		return 1;
+	}
+
+
+
+
+	return 0;
+
+}
---
+++ testbench/codegen_driver/unpack_arg_slave.c
@@ -0,0 +1,15 @@
+float f(float a,float b){
+	return a + 5;
+}
+
+float g(float a,float b){
+	return a + b;
+}
+
+float h(float a,int b, float c){
+	return a + b + c;
+}
+
+float i(int a, float b, float c, int d, float e){
+	return a + b + c + d + e;
+}
