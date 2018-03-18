69b721c47c89d8931af2a86ddd4bce7c0f3dfdb6
patrickjohncyh <patrickjohncyh@icloud.com>
2018-03-18 14:36:09 +0000

    Add testcase to check for correct argument packing of floats

--- testbench/codegen_driver/argument_packing.driver.c
+++ testbench/codegen_driver/argument_packing.driver.c
@@ -13,3 +13,7 @@ float h(float a,int b, float c){
 float i(int a, float b, float c, int d, float e){
 	return a*2 + b/3 + c-4 + d*1.5 + e/2;
 }
+
+float j(float a, float b, float c, float d, float e){
+	return a+b+c+d+e;
+}
--- testbench/codegen_driver/argument_packing.slave.c
+++ testbench/codegen_driver/argument_packing.slave.c
@@ -2,6 +2,7 @@ float f(float a);
 float g(float a,float b);
 float h(float a,int b, float c);
 float i(int a, float b, float c, int d, float e);
+float j(float a, float b, float c, float d, float e);
 
 
 int main(){
@@ -9,6 +10,9 @@ int main(){
 	float a = 1.23;
 	float b = 4.5;
 	int c = 4;
+	float x = 143.4;
+	float y = 9.7485;
+	float z = 0.134;
 
 	if(f(a) != a + 5){
 		return 1;
@@ -19,8 +23,10 @@ int main(){
 	if(h(a,c,b) != a*2+c*3+b*4){
 		return 1;
 	}
-
 	if(i(c,a,a,c,b) !=  c*2+a/3+a-4+c*1.5+b/2 ){
+		return 1;
+	}
+	if(j(a,b,x,y,z) != a+b+x+y+z){
 		return 1;
 	}
 
